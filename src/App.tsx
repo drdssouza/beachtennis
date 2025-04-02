a9e27b662b6b67dad89e64b7ee968c98d39c225c
drdssouza <eduardoschrotke@gmail.com>
2025-04-02 11:02:38 -0300

    adicionando att

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
-const getBasename = () => {
-  const publicUrl = import.meta.env.BASE_URL || '/';
-  return publicUrl.endsWith('/') ? publicUrl.slice(0, -1) : publicUrl;
-};
 
 const queryClient = new QueryClient();
 
@@ -18,14 +13,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={getBasename()}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="/404" element={<NotFound />} />
           <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
