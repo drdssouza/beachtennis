ee7cdd0fa4fd6093ed0920473e41441b956e5f9b
drdssouza <eduardoschrotke@gmail.com>
2025-04-02 10:55:46 -0300

    Adicionando arquivos necessarios

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+
+const getBasename = () => {
+  const publicUrl = import.meta.env.BASE_URL || '/';
+  return publicUrl.endsWith('/') ? publicUrl.slice(0, -1) : publicUrl;
+};
 
 const queryClient = new QueryClient();
 
@@ -13,11 +18,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={getBasename()}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
